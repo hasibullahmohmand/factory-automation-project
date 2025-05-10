c3e05296d7a36974daeb6a92f0d5fc86120f2695
hasibullahmohmand <hasibullah.mohmand14@gmail.com>
2025-05-10 22:35:42 +0300

    elements added

--- frontend/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd62c6c0f50c1a32ef58bd709c1f214e15459a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e487601b0bac9a7e83ed85df2ca003c97c06f7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
