9f654419d71facfad0283e78b5e3e411f6cce469
hasibullahmohmand <hasibullah.mohmand14@gmail.com>
2025-05-17 12:26:38 +0300

    Order page added

--- frontend/FactoryProject/FactoryProject/Models/OrderDtos/ResultOrderDto.cs
+++ frontend/FactoryProject/FactoryProject/Models/OrderDtos/ResultOrderDto.cs
@@ -6,4 +6,6 @@ public class ResultOrderDto
     public ResultCartDto? cart { get; set; }
     public DateTime deliveryDate { get; set; }
     public bool delivered { get; set; } = false;
+
+    public DateTime orderDate { get; set; }
 }
